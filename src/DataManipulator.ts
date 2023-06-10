59101b7d5de3b0dd28ebb708240f3b988b530707
SeaPandaCodes <seapandaramen@gmail.com>
2023-06-10 19:20:43 -0400

    Created Alert Graph

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,37 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const stockABC = serverResponds[0];
+    const stockEFG = serverResponds[1];
+    const priceABC = (stockABC.top_ask.price + stockABC.top_bid.price) / 2;
+    const priceEFG = (stockEFG.top_ask.price + stockEFG.top_bid.price) / 2;
+    const ratio = priceABC / priceEFG;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+
+    return {
+      price_abc: priceABC,
+      price_def: priceEFG,
+      ratio: ratio,
+      timestamp:
+        stockABC.timestamp > stockEFG.timestamp
+          ? stockABC.timestamp
+          : stockEFG.timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert:
+        ratio > upperBound || ratio < lowerBound ? ratio : undefined,
+    };
   }
 }
